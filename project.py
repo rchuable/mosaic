3b8fd646775b6526c3fa9e22a65bf60696b7217c
Regina Chua <19302573+rchuable@users.noreply.github.com>
2024-12-20 18:17:26 -0500

    fixed container width

--- project.py
+++ project.py
@@ -35,14 +35,14 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
     image_url = st.text_input("Or enter an image URL")
     if image_url:
         try:
             response = requests.get(image_url)
             image = Image.open(BytesIO(response.content))
-            st.image(image, caption="Image from URL", use_column_width=True)
+            st.image(image, caption="Image from URL", use_container_width=True)
         except Exception as e:
             st.error("Error loading image from URL.")
 
