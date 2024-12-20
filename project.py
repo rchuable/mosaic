27821fdf57114b2ea14cb790af6b204338479eac
Regina Chua <19302573+rchuable@users.noreply.github.com>
2024-12-20 18:30:43 -0500

    image preview

--- project.py
+++ project.py
@@ -32,7 +32,60 @@ from io import BytesIO
 def main():
     st.title("Mosaic Maker - app developed by Regina Chua")
 
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg", "png"])
+    # Initialize
+    if 'images' not in st.session_state:
+        st.session_state.images = []
+
+    # Image upload
+    uploaded_files = st.file_uploader("Choose an image...", type=["jpg","jpeg", "png"], accept_multiple_files=True)
+    
+    
+    # Image URL
+    image_urls = st.text_area("Or enter image URLS (one per line)")
+
+    images = []
+
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            image = Image.open(uploaded_file)
+            images.append(image)
+    
+    if image_urls:
+        urls = image_urls.split('\n')
+        for url in urls:
+            try:
+                response = requests.get(url)
+                image = Image.open(BytesIO(response.content))
+                images.append(image)
+            except Exception as e:
+                st.error(f"Error loading image from URL: {url}. Error: {e}")
+    if images:
+        st.subheader("Image Preview")
+        cols = st.columns(3)
+        for i, img in enumerate(images):
+            col = cols[i % 3]
+            col.image(img, use_container_width=True)
+    
+    title = st.text_input("Name the image (optional):", value="mosaic.png")
+
+    if st.button("Make Mosaic"):
+        if images:
+            collage = create_mosaic(images)
+            st.image(collage, caption=title)
+
+            img_byte_arr = BytesIO()
+            collage.save(img_byte_arr, format='PNG')
+            img_byte_arr = img_byte_arr.getvalue()
+            st.download_button(label="Download Collage", data=img_byte_arr, file_name=title, mime="image/png")
+        else:
+            st.error("Please upload at least one image.")
+
+def create_mosaic(images):
+        pass
+
+if __name__ == "__main__":
+    main() 
+'''
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption="Uploaded Image", use_container_width=True)
@@ -46,5 +99,4 @@ def main():
         except Exception as e:
             st.error("Error loading image from URL.")
 
-if __name__ == "__main__":
-    main()
+'''
